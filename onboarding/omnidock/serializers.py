879c345dbdda00aa9e95701deacf8c073aca3d03
paulburkhardt <92929336+paulburkhardt@users.noreply.github.com>
2024-06-27 08:32:11 +0200

    .

--- onboarding/omnidock/serializers.py
+++ onboarding/omnidock/serializers.py
@@ -1,18 +1,47 @@
 from rest_framework import serializers
-from .models import Order, Product, PositionItem
+from .models import Order, Product, PositionItem, ItemValueGrossPrice, TrackingInfo
 
-class ProductSalesSerializer(serializers.ModelSerializer):
-    total_sales = serializers.IntegerField()
-    total_revenue = serializers.DecimalField(max_digits=10, decimal_places=2)
+class ItemValueGrossPriceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemValueGrossPrice
+        fields = ['amount', 'currency']
 
+
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['sku', 'product_title', 'total_sales', 'total_revenue']
+        fields = ['sku', 'product_title', 'omnidock_article_number']
 
-class OrderFulfillmentSerializer(serializers.ModelSerializer):
-    shipping_date = serializers.DateTimeField()
-    order_status = serializers.CharField()
-    
+
+class TrackingInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrackingInfo
+        fields = ['carrier', 'tracking_number']
+
+class PositionItemSerializer(serializers.ModelSerializer):
+    item_value_gross_price = ItemValueGrossPriceSerializer()
+    product = ProductSerializer()
+    tracking_info = TrackingInfoSerializer()
+
+    class Meta:
+        model = PositionItem
+        fields = ['position_item_id', 'fulfillment_status', 'sentDate', 'item_value_gross_price', 'product', 'tracking_info']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    position_items = serializers.SerializerMethodField()
+
     class Meta:
         model = Order
-        fields = ['marketplace_order_id', 'omnidock_order_id', 'order_date', 'shipping_date', 'order_status']
+        fields = ['marketplace_order_id', 'omnidock_order_id', 'order_date', 'shipping_date', 'position_items']
+
+    def get_position_items(self, obj):
+        # Fetching only the fulfilled position items
+        fulfilled_items = PositionItem.objects.filter(order=obj, fulfillment_status='fulfilled')
+        return PositionItemSerializer(fulfilled_items, many=True).data
+
+class ProductSalesSerializer(serializers.Serializer):
+    sku = serializers.CharField(max_length=100)
+    product_title = serializers.CharField(max_length=255)
+    sold_count = serializers.IntegerField()
+    gross_revenue = serializers.DecimalField(max_digits=10, decimal_places=2)
--- onboarding/omnidock/views.py
+++ onboarding/omnidock/views.py
@@ -1,24 +1,29 @@
 from django.db.models import Sum, Count, F
 from rest_framework import generics
-from rest_framework.response import Response
 from .models import Order, Product, PositionItem
-from .serializers import ProductSalesSerializer, OrderFulfillmentSerializer
+from .serializers import OrderSerializer, ProductSalesSerializer 
 
-# class ProductSalesListView(generics.ListAPIView):
-#     serializer_class = ProductSalesSerializer
+class ProductSalesListView(generics.ListAPIView):
+    serializer_class = ProductSalesSerializer
 
-#     def get_queryset(self):
-#         return Product.objects.annotate(
-#             total_sales=Count('positionitem'),
-#             total_revenue=Sum('positionitem__item_value_gross_price__amount')
-#         )
-# 
-# class OrderFulfillmentListView(generics.ListAPIView):
-#     serializer_class = OrderFulfillmentSerializer
+    def get_queryset(self):
+        queryset = (
+            Product.objects
+            .annotate(
+                sold_count=Count('positionitem'),
+                gross_revenue=Sum(F('positionitem__item_value_gross_price__amount'))
+            )
+            .values('sku', 'product_title', 'sold_count', 'gross_revenue')
+        )
+        return queryset
 
-#     def get_queryset(self):
-#         return Order.objects.annotate(
-#             order_status=F('positionitem__fulfillment_status')
-#         ).filter(
-#             positionitem__fulfillment_status__in=['Ready to Fulfill', 'Fulfilled']
-#         ).distinct()
+class FullFilledOrdersView(generics.ListAPIView):
+    serializer_class = OrderSerializer
+    def get_queryset(self):
+        queryset = (
+            Order.objects
+            .filter(positionitem__fulfillment_status='fulfilled')
+            .values('omnidock_order_id', 'order_date', 'shipping_date')
+            .distinct()
+        )
+        return queryset
